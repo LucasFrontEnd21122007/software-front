85fd1bdeca6c1c5263edd52d483c0e1e36df74a3
LUCAS MARTINEZ <132151483+LucasFrontEnd21122007@users.noreply.github.com>
2023-10-22 11:58:56 -0300

    arquivos do projeto
    arquivos do projeto

---
+++ index.jsx
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Meu Projeto React com SCSS</h1>
+    </div>
+  );
+}
+
+export default App;
